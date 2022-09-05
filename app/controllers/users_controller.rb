8fd61ba83dd9a6002c7e74476a76eb88be402c7e
takathanda538 <106585979+takathanda538@users.noreply.github.com>
2022-09-05 02:59:13 +0000

    [応用b][finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     
+    @books_count = @books.group_by_day(:created_at).size
+    @user_today = @books.where(created_at: Date.today.all_day).count
+    
     @today_book = @books.created_today
     @yesterday_book = @books.created_yesterday
     @this_week_book = @books.created_this_week
@@ -62,6 +65,18 @@ class UsersController < ApplicationController
       redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
       render "edit"
+    end
+  end
+  
+  def search
+    @user = User.find(params[:user_id])
+    @books = @user.books 
+    @book = Book.new
+    if params[:created_at] == ""
+      @search_book = "日付を選択してください"#①
+    else
+      create_at = params[:created_at]
+      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count#②
     end
   end
 
